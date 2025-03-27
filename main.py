c321a0ee0c8a7d943fafbbdbbd4711a621976502
haleytri <124080734+haleytri@users.noreply.github.com>
2025-03-27 13:22:20 -0500

    set up

---
+++ main.py
@@ -0,0 +1,23 @@
+'''
+The Data Divas:
+    Anya Shrestha
+    Elena Rodriguez
+    Haley Trinh
+    Lendy Varela
+
+COSC 4337 - Data Science II
+'''
+
+import tensorflow as tf
+import matplotlib as mp
+import numpy as np
+import pandas as pd
+import seaborn as sb
+import time
+
+def main():
+    batch_size = 0
+    epoch = 0
+
+if __name__ == "__main__":
+    main()
