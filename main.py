82447a59566826c9a08bffa7516045b90e19b605
anyas <anyashrestha@gmail.com>
2025-04-09 23:00:35 -0500

    Initial code main approach CNN

--- main.py
+++ main.py
@@ -9,15 +9,84 @@ COSC 4337 - Data Science II
 '''
 
 import tensorflow as tf
-import matplotlib as mp
+from tensorflow.keras import datasets, layers, models
+from tensorflow.keras.preprocessing import image_dataset_from_directory
+import matplotlib as plt
 import numpy as np
 import pandas as pd
 import seaborn as sb
 import time
 
+def preprocess_image(image, label):
+    image = tf.image.resize_with_pad(image,224,224)
+    image = tf.cast(image, tf.float32) / 255.0
+    return(image, label)
+
 def main():
-    batch_size = 0
-    epoch = 0
+    data_dir = 'dataset'
+    # Load dataset
+    batch_size = 32
+    img_size = (224, 224)  
+
+    train_ds = image_dataset_from_directory(
+        data_dir,
+        validation_split=0.2,
+        subset="training",
+        seed=123,
+        shuffle=True,
+        image_size=img_size,
+        batch_size=batch_size
+    )
+    
+    val_ds = image_dataset_from_directory(
+        data_dir,
+        validation_split=0.2,
+        subset="validation",
+        seed=123,
+        shuffle=True,
+        image_size=img_size,
+        batch_size=batch_size
+    )
+
+    # Preprocessing
+    train_ds = train_ds.map(preprocess_image)
+    val_ds = val_ds.map(preprocess_image)
+
+    AUTOTUNE = tf.data.AUTOTUNE
+    train_ds = train_ds.prefetch(buffer_size=AUTOTUNE)
+    val_ds = val_ds.prefetch(buffer_size=AUTOTUNE)
+
+    #CNN(?)
+    model = models.Sequential([
+        layers.Conv2D(32, (3, 3), activation='relu', input_shape=(224, 224, 3)),
+        layers.MaxPooling2D((2, 2)),
+        layers.Conv2D(64, (3, 3), activation='relu'),
+        layers.MaxPooling2D((2, 2)),
+        layers.Conv2D(128, (3, 3), activation='relu'),
+        layers.MaxPooling2D((2, 2)),
+        layers.Flatten(),
+        layers.Dense(128, activation='relu'),
+        layers.Dense(10, activation='softmax')  # 10 dog breeds
+    ])
+
+    # Compile 
+    model.compile(optimizer='adam',
+                loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                metrics=['accuracy'])
+
+    history = model.fit(train_ds, validation_data=val_ds, epochs=10)
+    
+    # Evaluate
+    plt.plot(history.history['accuracy'], label='accuracy')
+    plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.ylim([0.5, 1])
+    plt.legend(loc='lower right')
+
+    test_loss, test_acc = model.evaluate(val_ds)
+    print('\nValidation accuracy:', test_acc)
+
 
 if __name__ == "__main__":
     main()
