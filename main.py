7acbc3275869d2a43fe58711346666ae65b48e78
haleytri <124080734+haleytri@users.noreply.github.com>
2025-04-22 23:20:30 -0500

    some testing! comparing diff models + adding loss

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ COSC 4337 - Data Science II
 import tensorflow as tf
 from tensorflow.keras import datasets, layers, models
 from tensorflow.keras.preprocessing import image_dataset_from_directory
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sb
@@ -26,11 +26,11 @@ def main():
     data_dir = 'dataset'
     # Load dataset
     batch_size = 32
-    img_size = (224, 224)  
+    img_size = (224, 224) 
 
     train_ds = image_dataset_from_directory(
         data_dir,
-        validation_split=0.2,
+        validation_split=0.3,
         subset="training",
         seed=123,
         shuffle=True,
@@ -40,7 +40,7 @@ def main():
     
     val_ds = image_dataset_from_directory(
         data_dir,
-        validation_split=0.2,
+        validation_split=0.3,
         subset="validation",
         seed=123,
         shuffle=True,
@@ -56,33 +56,115 @@ def main():
     train_ds = train_ds.prefetch(buffer_size=AUTOTUNE)
     val_ds = val_ds.prefetch(buffer_size=AUTOTUNE)
 
-    #CNN(?)
+    #CNN
     model = models.Sequential([
         layers.Conv2D(32, (3, 3), activation='relu', input_shape=(224, 224, 3)),
         layers.MaxPooling2D((2, 2)),
+
         layers.Conv2D(64, (3, 3), activation='relu'),
         layers.MaxPooling2D((2, 2)),
+
         layers.Conv2D(128, (3, 3), activation='relu'),
         layers.MaxPooling2D((2, 2)),
+
+        layers.Conv2D(256, (3, 3), activation='relu'),
+        layers.MaxPooling2D((2, 2)),
+
         layers.Flatten(),
         layers.Dense(128, activation='relu'),
         layers.Dense(10, activation='softmax')  # 10 dog breeds
     ])
+    '''model = models.Sequential([
+        layers.Conv2D(32, (3, 3), activation='relu', input_shape=(224, 224, 3)),
+        layers.MaxPooling2D((2, 2)),
+
+        layers.Conv2D(64, (3, 3), activation='relu'),
+        layers.MaxPooling2D((2, 2)),
+
+        layers.Flatten(),
+        layers.Dense(128, activation='relu'),
+        layers.Dropout(0.5),
+        layers.Dense(10, activation='softmax')  # 10 dog breeds
+    ])'''
+
+    #original model
+    '''model = models.Sequential([
+        layers.Conv2D(32, (3, 3), activation='relu', input_shape=(224, 224, 3)),
+        layers.MaxPooling2D((2, 2)),
+
+        layers.Conv2D(64, (3, 3), activation='relu'),
+        layers.MaxPooling2D((2, 2)),
+
+        layers.Conv2D(128, (3, 3), activation='relu'),
+        layers.MaxPooling2D((2, 2)),
+
+        layers.Conv2D(256, (3, 3), activation='relu'),
+        layers.MaxPooling2D((2, 2)),
+
+        layers.Flatten(),
+        layers.Dense(128, activation='relu'),
+        layers.Dense(10, activation='softmax')  # 10 dog breeds
+    ])'''
+    #model w/ 4 layers
+    '''model = models.Sequential([
+        layers.Conv2D(32, (3, 3), activation='relu', input_shape=(224, 224, 3)),
+        layers.MaxPooling2D((2, 2)),
+
+        layers.Conv2D(64, (3, 3), activation='relu'),
+        layers.MaxPooling2D((2, 2)),
+
+        layers.Conv2D(128, (3, 3), activation='relu'),
+        layers.MaxPooling2D((2, 2)),
+
+        layers.Conv2D(256, (3, 3), activation='relu'),
+        layers.MaxPooling2D((2, 2)),
+
+        layers.Flatten(),
+        layers.Dense(128, activation='relu'),
+        layers.Dense(10, activation='softmax')  # 10 dog breeds
+    ])'''
+
+    #model w/ dropout
+    '''model = models.Sequential([
+        layers.Conv2D(32, (3, 3), activation='relu', input_shape=(224, 224, 3)),
+        layers.MaxPooling2D((2, 2)),
+
+        layers.Conv2D(64, (3, 3), activation='relu'),
+        layers.MaxPooling2D((2, 2)),
+
+        layers.Conv2D(128, (3, 3), activation='relu'),
+        layers.MaxPooling2D((2, 2)),
+
+        layers.Flatten(),
+        layers.Dense(128, activation='relu'),
+        layers.Dropout(0.5),
+        layers.Dense(10, activation='softmax')  # 10 dog breeds
+    ])'''
 
     # Compile 
     model.compile(optimizer='adam',
-                loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
                 metrics=['accuracy'])
 
-    history = model.fit(train_ds, validation_data=val_ds, epochs=10)
+    history = model.fit(train_ds, validation_data=val_ds, epochs=20)
     
     # Evaluate
     plt.plot(history.history['accuracy'], label='accuracy')
     plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+    plt.title('Accuracy vs Epochs')
     plt.xlabel('Epoch')
     plt.ylabel('Accuracy')
     plt.ylim([0.5, 1])
     plt.legend(loc='lower right')
+    plt.show()
+
+    plt.plot(history.history['loss'], label='Training Loss')
+    plt.plot(history.history['val_loss'], label='Validation Loss')
+    plt.title('Loss vs Epochs')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.legend(loc='lower right')
+    plt.show()
 
     test_loss, test_acc = model.evaluate(val_ds)
     print('\nValidation accuracy:', test_acc)
